26214b90115140e557e28e9d21a1901097d01483
kseni <kseniya.buko@gmail.com>
2021-04-01 09:58:41 +0200

    Homework2 List of Notes

--- app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
@@ -5,7 +5,7 @@ import io.techmeskills.an02onl_plannerapp.support.CoroutineViewModel
 class MainViewModel : CoroutineViewModel() {
 
     val notes = listOf(
-        Note("Помыть посуду"),
+        Note("Написать в налоговую"),
         Note("Забрать пальто из химчистки", "23.03.2021"),
         Note("Позвонить Ибрагиму"),
         Note("Заказать перламутровые пуговицы"),
@@ -15,7 +15,11 @@ class MainViewModel : CoroutineViewModel() {
         Note("Выбить ковры перед весной"),
         Note("Заклеить сапог жене"),
         Note("Купить картошки"),
-        Note("Скачать кино в самолёт", "25.03.2021")
+        Note("Скачать кино в самолёт", "25.03.2021"),
+
+        // added 2 Notes
+        Note("Пересадить цветы", "01.04.2021"),
+        Note("Заказать еду")
     )
 
 }
